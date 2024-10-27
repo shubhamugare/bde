002dcb64c3a7af20c0d2ee6449d55d00406335d2
Shubham Ugare <shubhamdugare@gmail.com>
2024-10-27 01:16:58 -0500

    Added contract comments

--- groups/bal/balber/balber_beruniversaltagnumber.cpp
+++ groups/bal/balber/balber_beruniversaltagnumber.cpp
@@ -14,6 +14,7 @@ namespace balber {
                         // ----------------------------
 
 // CLASS METHODS
+ res_tmp != NULL
 const char *BerUniversalTagNumber::toString(BerUniversalTagNumber::Value value)
 {
 #ifdef CASE
