0a024ce2e0925cebec7f201e6b5250f1a192f234
Shubham Ugare <shubhamdugare@gmail.com>
2024-10-27 01:19:05 -0500

    Added contract comments

--- groups/bal/balm/balm_defaultmetricsmanager.cpp
+++ groups/bal/balm/balm_defaultmetricsmanager.cpp
@@ -29,6 +29,7 @@ bslma::Allocator     *balm::DefaultMetricsManager::s_allocator_p = 0;
 
 namespace balm {
 // CLASS METHODS
+ res_tmp != NULL
 MetricsManager *DefaultMetricsManager::create(bslma::Allocator *basicAllocator)
 {
     BSLS_ASSERT(0 == s_singleton_p);
