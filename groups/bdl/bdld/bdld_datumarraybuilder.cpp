3c29ef28ff176097f85b4d5898f2d401a6a20a7d
Shubham Ugare <shubhamdugare@gmail.com>
2024-10-27 01:20:53 -0500

    Added contract comments

--- groups/bdl/bdld/bdld_datumarraybuilder.cpp
+++ groups/bdl/bdld/bdld_datumarraybuilder.cpp
@@ -22,6 +22,7 @@ typedef DatumArrayBuilder::allocator_type allocator_type;
 
 /// Calculate the new capacity needed to accommodate data having the
 /// specified `length` for the datum array having the specified `capacity`.
+ res_tmp >= length
 static DatumArrayBuilder::SizeType getNewCapacity(
                                           DatumArrayBuilder::SizeType capacity,
                                           DatumArrayBuilder::SizeType length)
