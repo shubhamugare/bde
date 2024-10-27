5fef475c2aea05d97449e16ae640640cf14385c8
Shubham Ugare <shubhamdugare@gmail.com>
2024-10-27 01:25:44 -0500

    Added contract comments

--- groups/bbl/bbldc/bbldc_basicactual360.cpp
+++ groups/bbl/bbldc/bbldc_basicactual360.cpp
@@ -14,6 +14,7 @@ namespace bbldc {
                           // ---------------------
 
 // CLASS METHODS
+ true
 double BasicActual360::yearsDiff(const bdlt::Date& beginDate,
                                  const bdlt::Date& endDate)
 {
