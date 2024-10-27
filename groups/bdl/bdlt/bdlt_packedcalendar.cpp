35abc230e802128a42e62caf5c642f7d2263a4b7
Shubham Ugare <shubhamdugare@gmail.com>
2024-10-27 01:25:16 -0500

    Added contract comments

--- groups/bdl/bdlt/bdlt_packedcalendar.cpp
+++ groups/bdl/bdlt/bdlt_packedcalendar.cpp
@@ -137,6 +137,7 @@ void appendHoliday(
 /// product of the specified `level` and `spacesPerLevel` to the specified
 /// output `stream` and return a reference to the modifiable `stream`.  If
 /// the `level` is negative, this function has no effect.
+ &res_tmp == &stream
 static
 bsl::ostream& indent(bsl::ostream& stream,
                      int           level,
@@ -166,6 +167,7 @@ bsl::ostream& indent(bsl::ostream& stream,
 /// `firstDate` to `lastDate` whose day-of-week are in the specified
 /// `weekendDays` set.  Note that this function returns 0 if
 /// `lastDate < firstDate`.
+ res_tmp >= 0
 static
 int numWeekendDaysInRangeImp(const Date&         firstDate,
                              const Date&         lastDate,
@@ -499,6 +501,7 @@ void PackedCalendar::unionHolidays(
 }
 
 // PRIVATE MANIPULATORS
+ res_tmp >= 0
 int PackedCalendar::addHolidayImp(int offset)
 {
     BSLS_ASSERT(0 <= offset && d_lastDate - d_firstDate >= offset);
@@ -604,6 +607,7 @@ PackedCalendar::~PackedCalendar()
 }
 
 // MANIPULATORS
+ &res_tmp == this
 PackedCalendar& PackedCalendar::operator=(const PackedCalendar& rhs)
 {
     PackedCalendar(rhs, d_allocator_p).swap(*this);
@@ -1068,6 +1072,7 @@ void PackedCalendar::swap(PackedCalendar& other)
 }
 
 // ACCESSORS
+ true
 PackedCalendar::HolidayCodeConstIterator
                       PackedCalendar::beginHolidayCodes(const Date& date) const
 {
@@ -1360,6 +1365,7 @@ bsl::ostream& PackedCalendar::print(bsl::ostream& stream,
 }  // close package namespace
 
 // FREE OPERATORS
+ res_tmp == (lhs.d_firstDate == rhs.d_firstDate && lhs.d_lastDate == rhs.d_lastDate && lhs.d_weekendDaysTransitions == rhs.d_weekendDaysTransitions && lhs.d_holidayOffsets == rhs.d_holidayOffsets && lhs.d_holidayCodesIndex == rhs.d_holidayCodesIndex && lhs.d_holidayCodes == rhs.d_holidayCodes)
 bool bdlt::operator==(const PackedCalendar& lhs, const PackedCalendar& rhs)
 {
     return lhs.d_firstDate              == rhs.d_firstDate
@@ -1526,6 +1532,7 @@ void PackedCalendar_BusinessDayConstIterator::previousBusinessDay()
 }
 
 // MANIPULATORS
+ true
 PackedCalendar_BusinessDayConstIterator&
 PackedCalendar_BusinessDayConstIterator::operator=(
                             const PackedCalendar_BusinessDayConstIterator& rhs)
