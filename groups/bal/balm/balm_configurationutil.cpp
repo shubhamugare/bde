8a04b3e81f492aedf9a54d49238902324f7c44ad
Shubham Ugare <shubhamdugare@gmail.com>
2024-10-27 01:19:14 -0500

    Added contract comments

--- groups/bal/balm/balm_configurationutil.cpp
+++ groups/bal/balm/balm_configurationutil.cpp
@@ -26,6 +26,7 @@ namespace balm {
                           // ------------------------
 
 // CLASS METHODS
+ res_tmp == 0 || res_tmp == -1
 int ConfigurationUtil::setFormat(const char          *category,
                                  const char          *metricName,
                                  const MetricFormat&  format,
