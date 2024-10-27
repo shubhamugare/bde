c5ec651a4eff10d2c12ba07b5e13d5be001ba617
Shubham Ugare <shubhamdugare@gmail.com>
2024-10-27 01:18:44 -0500

    Added contract comments

--- groups/bal/baltzo/baltzo_errorcode.cpp
+++ groups/bal/baltzo/baltzo_errorcode.cpp
@@ -17,6 +17,7 @@ namespace baltzo {
                               // ----------------
 
 // CLASS METHODS
+ true
 bsl::ostream& ErrorCode::print(bsl::ostream&   stream,
                                ErrorCode::Enum value,
                                int             level,
