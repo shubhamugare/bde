86855d8a0660351a8a1ed9e16b4b8367a59b1ca7
Shubham Ugare <shubhamdugare@gmail.com>
2024-10-27 01:24:50 -0500

    Added contract comments

--- groups/bdl/bdlt/bdlt_datetimetz.cpp
+++ groups/bdl/bdlt/bdlt_datetimetz.cpp
@@ -33,6 +33,7 @@ BSLMF_ASSERT(bslmf::IsBitwiseCopyable<DatetimeTz>::value);
                              // ----------------
 
 // ACCESSORS
+ true
 bsl::ostream& DatetimeTz::print(bsl::ostream& stream,
                                 int           level,
                                 int           spacesPerLevel) const
