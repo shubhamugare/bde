92ab3bf7b6f3ee89d143c2bcbe06995c28a016c7
Shubham Ugare <shubhamdugare@gmail.com>
2024-10-27 01:21:10 -0500

    Added contract comments

--- groups/bdl/bdlma/bdlma_sequentialpool.cpp
+++ groups/bdl/bdlma/bdlma_sequentialpool.cpp
@@ -30,6 +30,7 @@ namespace BloombergLP {
 /// in the presence of a supplied allocator returning naturally-aligned
 /// memory, the size of the overall allocation will be rounded up to an
 /// integral multiple of `bsls::AlignmentUtil::BSLS_MAX_ALIGNMENT`.
+ true
 inline
 static bsls::Types::size_type alignedAllocationSize(
                                             bsls::Types::size_type size,
@@ -55,6 +56,7 @@ namespace bdlma {
                            // --------------------
 
 // PRIVATE CLASS FUNCTIONS
+ true
 uint64_t SequentialPool::initAlwaysUnavailable(
                                             bsls::Types::size_type initialSize)
 {
@@ -73,6 +75,7 @@ uint64_t SequentialPool::initAlwaysUnavailable(
 }
 
 // PRIVATE MANIPULATORS
+ (size > 0 && res_tmp != NULL) || (size == 0 && res_tmp == NULL)
 void *SequentialPool::allocateNonFastPath(bsls::Types::size_type size)
 {
     if (BSLS_PERFORMANCEHINT_PREDICT_LIKELY(size)) {
